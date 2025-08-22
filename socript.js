9267a737ade998cbc3b65027fa277ddba657272f
shohjahon <xayrullayevshohjahon871@gmail.com>
2025-08-22 18:19:21 +0500

    Add files via upload

---
+++ socript.js
@@ -0,0 +1,13 @@
+let mevalar = ["olma", "anor", "olmacha", "qovun", ];
+console.log (mevalar);
+
+alert("Boshlanishida mevalar soni " + mevalar.length);
+
+
+if (result == true) {
+  mevalar.pop();
+} else {
+  mevalar.push("tarvuz");
+}
+
+console.log(mevalar);
